dc0e2c9fec7a8410a6e29b5fda98390958dcb6e5
Aapo Talvensaari <aapo.talvensaari@gmail.com>
2015-01-08 21:24:41 +0200

    Fixed a typo on lib name.

--- lib/resty/opjson.lua
+++ lib/resty/opjson.lua
@@ -45,7 +45,7 @@ enum json_typ json_type(const struct json_token*);
 enum json_typ json_num(json_number *num, const struct json_token *tok);
 void json_deq(struct json_token*);
 ]]
-local lib = ffi_load("ibopjson")
+local lib = ffi_load("libopjson")
 local n, b, t, p = ffi_new("json_number[1]"), ffi_new("json_char[256]"), ffi_new("struct json_token"), ffi_new("json_pair")
 local ok, newtab = pcall(require, "table.new")
 if not ok then newtab = function() return {} end end
