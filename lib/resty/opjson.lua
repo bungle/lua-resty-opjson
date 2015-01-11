7b951b8ce1e379c72914b6c08f1541d524cb3aa9
Aapo Talvensaari <aapo.talvensaari@gmail.com>
2015-01-11 23:49:36 +0200

    Binding from now on to @bungle/libopjson.

--- lib/resty/opjson.lua
+++ lib/resty/opjson.lua
@@ -3,89 +3,80 @@ local ffi          = require "ffi"
 local ffi_new      = ffi.new
 local ffi_cdef     = ffi.cdef
 local ffi_load     = ffi.load
+local ffi_typeof   = ffi.typeof
 local ffi_str      = ffi.string
 local null         = {}
 if ngx and ngx.null then
     null = ngx.null
 end
 ffi_cdef[[
-typedef int json_int;
-typedef unsigned char json_char;
-typedef unsigned long json_size;
-typedef double json_number;
-enum json_typ {
-    JSON_NONE,
-    JSON_OBJECT,
-    JSON_ARRAY,
-    JSON_NUMBER,
-    JSON_STRING,
-    JSON_TRUE,
-    JSON_FALSE,
-    JSON_NULL
-};
 struct json_token {
-    const json_char *str;
-    json_size len;
-    json_size children;
-};
-struct json_pair {
-    struct json_token name;
-    struct json_token value;
+    const unsigned char *str;
+    unsigned long len;
+    unsigned long children;
 };
 struct json_iter {
-    json_int depth;
-    json_int err;
+    int depth;
+    int err;
     const void **go;
-    const json_char *src;
-    json_size len;
+    const unsigned char *src;
+    unsigned long len;
 };
-struct json_iter json_begin(const json_char*, json_size);
-struct json_iter json_read(struct json_token*, const struct json_iter*);
-struct json_iter json_parse(struct json_pair*, const struct json_iter*);
-json_size json_cpy(json_char*, json_size, const struct json_token*);
-enum json_typ json_type(const struct json_token*);
-enum json_typ json_num(json_number *num, const struct json_token *tok);
-void json_deq(struct json_token*);
+void json_begin(const unsigned char*, unsigned long, struct json_iter*);
+void json_read(struct json_token*, struct json_iter*);
+void json_type(int *, const struct json_token*);
+void json_num(double *, const struct json_token*);
 ]]
-local lib = ffi_load("libopjson")
+local lib = ffi_load("/Users/bungle/Sources/lua-resty-opjson/lib/resty/libopjson.so")
 local ok, newtab = pcall(require, "table.new")
 if not ok then newtab = function() return {} end end
 local arr  = { __index = { __jsontype = "array"  }}
 local obj  = { __index = { __jsontype = "object" }}
-local num  = ffi_new("json_number[1]")
-local buf = ffi_new("json_char[256]")
-local t,p = ffi_new("struct json_token"), ffi_new("struct json_pair")
-local val = newtab(8, 0)
-val[0] = function() return nil end
-val[1] = function(v)
-    local i, l = lib.json_begin(v.str, v.len), tonumber(v.children)
-    local o = setmetatable(newtab(0, l), obj)
-    for n = 1, l do
-        i = lib.json_parse(p, i)
-        o[val[4](p.name)] = val[tonumber(lib.json_type(p.value))](p.value)
+local itr = ffi_typeof("struct json_iter")
+local k,v,n = ffi_new("struct json_token"), ffi_new("struct json_token"), ffi_new("double[1]")
+local function val(p)
+    if p.str[0] == 123 then
+        local i = ffi_new(itr)
+        local l = tonumber(p.children)
+        local o = setmetatable(newtab(0, l), obj)
+        i.src, i.len = p.str, p.len
+        for j = 1, l do
+            lib.json_read(k, i)
+            lib.json_read(v, i)
+            o[ffi_str(k.str + 1, k.len - 2)] = val(v)
+        end
+        return o
     end
-    return o
-end
-val[2] = function(v)
-    local i, l = lib.json_begin(v.str, v.len), tonumber(v.children)
-    local a = setmetatable(newtab(l, 0), arr)
-    for n = 1, l do
-        i = lib.json_read(t, i)
-        a[n] = val[tonumber(lib.json_type(t))](t)
+    if p.str[0] == 91 then
+        local i = ffi_new(itr)
+        local l = tonumber(p.children)
+        local a = setmetatable(newtab(l, 0), arr)
+        i.src = p.str
+        i.len = p.len
+        for j = 1, l do
+            lib.json_read(v, i)
+            a[j] = val(v)
+        end
+        return a
     end
-    return a
+    if p.str[0] == 34  then return ffi_str(p.str + 1, p.len - 2) end
+    if p.str[0] == 116 then return true  end
+    if p.str[0] == 102 then return false end
+    if p.str[0] == 110 then return null  end
+    lib.json_num(n, p)
+    return n[0]
 end
-val[3] = function(v) lib.json_num(num, v); return tonumber(num[0]) end
-val[4] = function(v) lib.json_deq(v); return ffi_str(buf, lib.json_cpy(buf, tonumber(v.len) + 1, v)) end
-val[5] = function() return true  end
-val[6] = function() return false end
-val[7] = function() return null  end
 return function(j, l)
-    local i = lib.json_parse(p, lib.json_begin(j, l or #j))
+    local i = ffi_new(itr)
+    i.src = j
+    i.len = l or #j
+    lib.json_read(k, i)
+    lib.json_read(v, i)
     local o = {}
     while i.src ~= nil do
-        o[val[4](p.name)] = val[tonumber(lib.json_type(p.value))](p.value)
-        i = lib.json_parse(p, i)
+        o[ffi_str(k.str + 1, k.len - 2)] = val(v)
+        lib.json_read(k, i)
+        lib.json_read(v, i)
     end
     return o
 end
